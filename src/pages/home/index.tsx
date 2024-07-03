be50cbc0430afa4e2fa01a48a9a7b8658664932a
wenchangping <wenchangping@chainz.vip>
2024-07-03 15:09:12 +0800

    优化

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,6 +5,9 @@ import PaPlayLists from "@/components/Personalized/PaPlayLists";
 import { useState } from "react";
 import { Button, Drawer } from "antd";
 
+const images = import.meta.glob("../../assets/images/banners/*", { eager: true, import: "default" });
+const res = Object.entries(images).map(([name, src]) => <img src={src as string} key={name} />);
+
 function _App() {
 	const [open, setOpen] = useState(true);
 	const onClose = () => {
@@ -17,6 +20,8 @@ function _App() {
 	return (
 		<>
 			<Banner />
+
+			<div>{res}</div>
 			{/* 公共推荐 */}
 			<PaPlayLists />
 
